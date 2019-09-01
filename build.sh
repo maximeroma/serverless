3f5ad12c170de9e04feeace324dcf5e528ec6159
maximeroma <m.roma@techexmachina.com>
2019-09-01 11:33:03 +0200

    add secrets

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ elif [ "$1" = "deploy" ] && [ $# -eq 2 ]; then
   STAGE=$2
 
   npm install
-  'node_modules/.bin/sls' deploy -s $STAGE
+  'node_modules/.bin/sls' deploy -s $STAGE --verbose
 else
   instruction
   exit 1
